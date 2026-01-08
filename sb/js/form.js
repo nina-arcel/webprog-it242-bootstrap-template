0578c730a1cc2bfcb1c3c32a4d66888f8b43389d
Niña Arcel R. Ramos <nrramos@student.apc.edu.ph>
2026-01-09 02:32:21 +0800

    edit form js

--- sb/js/form.js
+++ sb/js/form.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
         userName: '',
         userComment: '',
         isYes: false,
-        responses:       }
+        responses: []
+      }
     },
     methods: {
       addEntry(){
